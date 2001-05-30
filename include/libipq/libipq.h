ef798b9b7e005209deb457d1ffccab3d2bcdba96
James Morris <jmorris@intercode.com.au>
2001-05-30 00:48:03 +0000

    Added more specific copyright & author information.

--- include/libipq/libipq.h
+++ include/libipq/libipq.h
@@ -3,6 +3,10 @@
  *
  * IPQ library for userspace.
  *
+ * Author: James Morris <jmorris@intercode.com.au>
+ *
+ * Copyright (c) 2000-2001 Netfilter Core Team
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
--- libipq/libipq.c
+++ libipq/libipq.c
@@ -6,6 +6,10 @@
  * Please note that this library is still developmental, and there may
  * be some API changes.
  *
+ * Author: James Morris <jmorris@intercode.com.au>
+ *
+ * Copyright (c) 2000-2001 Netfilter Core Team
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
