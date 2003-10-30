1d095f8df6570b832b4c95d89e9a98541e85f4c7
Yasuyuki KOZAKAI <yasuyuki.kozakai@toshiba.co.jp>
2003-10-30 06:36:49 +0000

    Fix invalid port number (Yasuyuki Kozakai)

--- extensions/libipt_MASQUERADE.c
+++ extensions/libipt_MASQUERADE.c
@@ -47,7 +47,7 @@ parse_ports(const char *arg, struct ip_nat_multi_range *mr)
 	mr->range[0].flags |= IP_NAT_RANGE_PROTO_SPECIFIED;
 
 	port = atoi(arg);
-	if (port == 0 || port > 65535)
+	if (port <= 0 || port > 65535)
 		exit_error(PARAMETER_PROBLEM, "Port `%s' not valid\n", arg);
 
 	dash = strchr(arg, '-');
