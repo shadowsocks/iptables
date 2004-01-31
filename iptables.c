617b7dd9ef2870e85694ad5212c61fa7c5986ec9
Karsten Desler <kdesler@soohrt.org>
2004-01-31 15:14:38 +0000

    Fix another possibly not zero-terminated string after copy (Karsten Desler)

--- iptables.c
+++ iptables.c
@@ -634,6 +634,7 @@ parse_hostnetworkmask(const char *name, struct in_addr **addrpp,
 	int i, j, k, n;
 
 	strncpy(buf, name, sizeof(buf) - 1);
+	buf[sizeof(buf) - 1] = '\0';
 	if ((p = strrchr(buf, '/')) != NULL) {
 		*p = '\0';
 		addrp = parse_mask(p + 1);
