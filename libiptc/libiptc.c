feca0578a5d035122b4b7cdb8d44d6cca819f35c
Robert de Barth <list-netfilter@debarth.co.uk>
2005-07-31 07:04:59 +0000

    _really_ sort only user defined chains (Robert de Barth <list-netfilter@debarth.co.uk>

--- libiptc/libiptc.c
+++ libiptc/libiptc.c
@@ -399,7 +399,7 @@ static inline void iptc_insert_chain(TC_HANDLE_T h, struct chain_head *c)
 	/* sort only user defined chains */
 	if (!c->hooknum) {
 		list_for_each_entry(tmp, &h->chains, list) {
-			if (strcmp(c->name, tmp->name) <= 0) {
+			if (!tmp->hooknum && strcmp(c->name, tmp->name) <= 0) {
 				list_add(&c->list, tmp->list.prev);
 				return;
 			}
