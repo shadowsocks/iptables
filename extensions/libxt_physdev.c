fec77fed67feb55aba4c33ae2367178c57ce83de
Yasuyuki KOZAKAI <yasuyuki@netfilter.org>
2007-07-24 07:06:57 +0000

    Unifies libip[6]t_physdev.c into libxt_physdev.c

--- extensions/libxt_physdev.c
+++ extensions/libxt_physdev.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <ctype.h>
-#include <ip6tables.h>
-#include <linux/netfilter_ipv6/ip6t_physdev.h>
+#include <xtables.h>
+#include <linux/netfilter/xt_physdev.h>
 #if defined(__GLIBC__) && __GLIBC__ == 2
 #include <net/ethernet.h>
 #else
@@ -45,62 +45,62 @@ parse(int c, char **argv, int invert, unsigned int *flags,
       unsigned int *nfcache,
       struct xt_entry_match **match)
 {
-	struct ip6t_physdev_info *info =
-		(struct ip6t_physdev_info*)(*match)->data;
+	struct xt_physdev_info *info =
+		(struct xt_physdev_info*)(*match)->data;
 
 	switch (c) {
 	case '1':
-		if (*flags & IP6T_PHYSDEV_OP_IN)
+		if (*flags & XT_PHYSDEV_OP_IN)
 			goto multiple_use;
 		check_inverse(optarg, &invert, &optind, 0);
 		parse_interface(argv[optind-1], info->physindev,
 				(unsigned char *)info->in_mask);
 		if (invert)
-			info->invert |= IP6T_PHYSDEV_OP_IN;
-		info->bitmask |= IP6T_PHYSDEV_OP_IN;
-		*flags |= IP6T_PHYSDEV_OP_IN;
+			info->invert |= XT_PHYSDEV_OP_IN;
+		info->bitmask |= XT_PHYSDEV_OP_IN;
+		*flags |= XT_PHYSDEV_OP_IN;
 		break;
 
 	case '2':
-		if (*flags & IP6T_PHYSDEV_OP_OUT)
+		if (*flags & XT_PHYSDEV_OP_OUT)
 			goto multiple_use;
 		check_inverse(optarg, &invert, &optind, 0);
 		parse_interface(argv[optind-1], info->physoutdev,
 				(unsigned char *)info->out_mask);
 		if (invert)
-			info->invert |= IP6T_PHYSDEV_OP_OUT;
-		info->bitmask |= IP6T_PHYSDEV_OP_OUT;
-		*flags |= IP6T_PHYSDEV_OP_OUT;
+			info->invert |= XT_PHYSDEV_OP_OUT;
+		info->bitmask |= XT_PHYSDEV_OP_OUT;
+		*flags |= XT_PHYSDEV_OP_OUT;
 		break;
 
 	case '3':
-		if (*flags & IP6T_PHYSDEV_OP_ISIN)
+		if (*flags & XT_PHYSDEV_OP_ISIN)
 			goto multiple_use;
 		check_inverse(optarg, &invert, &optind, 0);
-		info->bitmask |= IP6T_PHYSDEV_OP_ISIN;
+		info->bitmask |= XT_PHYSDEV_OP_ISIN;
 		if (invert)
-			info->invert |= IP6T_PHYSDEV_OP_ISIN;
-		*flags |= IP6T_PHYSDEV_OP_ISIN;
+			info->invert |= XT_PHYSDEV_OP_ISIN;
+		*flags |= XT_PHYSDEV_OP_ISIN;
 		break;
 
 	case '4':
-		if (*flags & IP6T_PHYSDEV_OP_ISOUT)
+		if (*flags & XT_PHYSDEV_OP_ISOUT)
 			goto multiple_use;
 		check_inverse(optarg, &invert, &optind, 0);
-		info->bitmask |= IP6T_PHYSDEV_OP_ISOUT;
+		info->bitmask |= XT_PHYSDEV_OP_ISOUT;
 		if (invert)
-			info->invert |= IP6T_PHYSDEV_OP_ISOUT;
-		*flags |= IP6T_PHYSDEV_OP_ISOUT;
+			info->invert |= XT_PHYSDEV_OP_ISOUT;
+		*flags |= XT_PHYSDEV_OP_ISOUT;
 		break;
 
 	case '5':
-		if (*flags & IP6T_PHYSDEV_OP_BRIDGED)
+		if (*flags & XT_PHYSDEV_OP_BRIDGED)
 			goto multiple_use;
 		check_inverse(optarg, &invert, &optind, 0);
 		if (invert)
-			info->invert |= IP6T_PHYSDEV_OP_BRIDGED;
-		*flags |= IP6T_PHYSDEV_OP_BRIDGED;
-		info->bitmask |= IP6T_PHYSDEV_OP_BRIDGED;
+			info->invert |= XT_PHYSDEV_OP_BRIDGED;
+		*flags |= XT_PHYSDEV_OP_BRIDGED;
+		info->bitmask |= XT_PHYSDEV_OP_BRIDGED;
 		break;
 
 	default:
@@ -125,68 +125,87 @@ print(const void *ip,
       const struct xt_entry_match *match,
       int numeric)
 {
-	struct ip6t_physdev_info *info =
-		(struct ip6t_physdev_info*)match->data;
+	struct xt_physdev_info *info =
+		(struct xt_physdev_info*)match->data;
 
 	printf("PHYSDEV match");
-	if (info->bitmask & IP6T_PHYSDEV_OP_ISIN)
+	if (info->bitmask & XT_PHYSDEV_OP_ISIN)
 		printf("%s --physdev-is-in",
-		       info->invert & IP6T_PHYSDEV_OP_ISIN ? " !":"");
-	if (info->bitmask & IP6T_PHYSDEV_OP_IN)
+		       info->invert & XT_PHYSDEV_OP_ISIN ? " !":"");
+	if (info->bitmask & XT_PHYSDEV_OP_IN)
 		printf("%s --physdev-in %s",
-		(info->invert & IP6T_PHYSDEV_OP_IN) ? " !":"", info->physindev);
+		(info->invert & XT_PHYSDEV_OP_IN) ? " !":"", info->physindev);
 
-	if (info->bitmask & IP6T_PHYSDEV_OP_ISOUT)
+	if (info->bitmask & XT_PHYSDEV_OP_ISOUT)
 		printf("%s --physdev-is-out",
-		       info->invert & IP6T_PHYSDEV_OP_ISOUT ? " !":"");
-	if (info->bitmask & IP6T_PHYSDEV_OP_OUT)
+		       info->invert & XT_PHYSDEV_OP_ISOUT ? " !":"");
+	if (info->bitmask & XT_PHYSDEV_OP_OUT)
 		printf("%s --physdev-out %s",
-		(info->invert & IP6T_PHYSDEV_OP_OUT) ? " !":"", info->physoutdev);
-	if (info->bitmask & IP6T_PHYSDEV_OP_BRIDGED)
+		(info->invert & XT_PHYSDEV_OP_OUT) ? " !":"", info->physoutdev);
+	if (info->bitmask & XT_PHYSDEV_OP_BRIDGED)
 		printf("%s --physdev-is-bridged",
-		       info->invert & IP6T_PHYSDEV_OP_BRIDGED ? " !":"");
+		       info->invert & XT_PHYSDEV_OP_BRIDGED ? " !":"");
 	printf(" ");
 }
 
 static void save(const void *ip, const struct xt_entry_match *match)
 {
-	struct ip6t_physdev_info *info =
-		(struct ip6t_physdev_info*)match->data;
+	struct xt_physdev_info *info =
+		(struct xt_physdev_info*)match->data;
 
-	if (info->bitmask & IP6T_PHYSDEV_OP_ISIN)
+	if (info->bitmask & XT_PHYSDEV_OP_ISIN)
 		printf("%s --physdev-is-in",
-		       info->invert & IP6T_PHYSDEV_OP_ISIN ? " !":"");
-	if (info->bitmask & IP6T_PHYSDEV_OP_IN)
+		       info->invert & XT_PHYSDEV_OP_ISIN ? " !":"");
+	if (info->bitmask & XT_PHYSDEV_OP_IN)
 		printf("%s --physdev-in %s",
-		(info->invert & IP6T_PHYSDEV_OP_IN) ? " !":"", info->physindev);
+		(info->invert & XT_PHYSDEV_OP_IN) ? " !":"", info->physindev);
 
-	if (info->bitmask & IP6T_PHYSDEV_OP_ISOUT)
+	if (info->bitmask & XT_PHYSDEV_OP_ISOUT)
 		printf("%s --physdev-is-out",
-		       info->invert & IP6T_PHYSDEV_OP_ISOUT ? " !":"");
-	if (info->bitmask & IP6T_PHYSDEV_OP_OUT)
+		       info->invert & XT_PHYSDEV_OP_ISOUT ? " !":"");
+	if (info->bitmask & XT_PHYSDEV_OP_OUT)
 		printf("%s --physdev-out %s",
-		(info->invert & IP6T_PHYSDEV_OP_OUT) ? " !":"", info->physoutdev);
-	if (info->bitmask & IP6T_PHYSDEV_OP_BRIDGED)
+		(info->invert & XT_PHYSDEV_OP_OUT) ? " !":"", info->physoutdev);
+	if (info->bitmask & XT_PHYSDEV_OP_BRIDGED)
 		printf("%s --physdev-is-bridged",
-		       info->invert & IP6T_PHYSDEV_OP_BRIDGED ? " !":"");
+		       info->invert & XT_PHYSDEV_OP_BRIDGED ? " !":"");
 	printf(" ");
 }
 
-static struct ip6tables_match physdev = {
+static struct xtables_match physdev = { 
+	.next		= NULL,
+	.family		= AF_INET,
 	.name		= "physdev",
 	.version	= IPTABLES_VERSION,
-	.size		= IP6T_ALIGN(sizeof(struct ip6t_physdev_info)),
-	.userspacesize	= IP6T_ALIGN(sizeof(struct ip6t_physdev_info)),
+	.size		= XT_ALIGN(sizeof(struct xt_physdev_info)),
+	.userspacesize	= XT_ALIGN(sizeof(struct xt_physdev_info)),
 	.help		= &help,
 	.init		= &init,
 	.parse		= &parse,
 	.final_check	= &final_check,
 	.print		= &print,
 	.save		= &save,
-	.extra_opts	= opts,
+	.extra_opts	= opts
+};
+
+static struct xtables_match physdev6 = { 
+	.next		= NULL,
+	.family		= AF_INET6,
+	.name		= "physdev",
+	.version	= IPTABLES_VERSION,
+	.size		= XT_ALIGN(sizeof(struct xt_physdev_info)),
+	.userspacesize	= XT_ALIGN(sizeof(struct xt_physdev_info)),
+	.help		= &help,
+	.init		= &init,
+	.parse		= &parse,
+	.final_check	= &final_check,
+	.print		= &print,
+	.save		= &save,
+	.extra_opts	= opts
 };
 
 void _init(void)
 {
-	register_match6(&physdev);
+	xtables_register_match(&physdev);
+	xtables_register_match(&physdev6);
 }
