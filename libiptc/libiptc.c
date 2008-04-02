2f93205b375ee9f5a383f8041749a9b989012dd0
Patrick McHardy <kaber@trash.net>
2008-04-02 14:01:53 +0200

    Retry ruleset dump when kernel returns EAGAIN.
    Bugzilla #104

--- libiptc/libiptc.c
+++ libiptc/libiptc.c
@@ -1226,7 +1226,7 @@ TC_INIT(const char *tablename)
 			return NULL;
 	}
 	sockfd_use++;
-
+retry:
 	s = sizeof(info);
 
 	strcpy(info.name, tablename);
@@ -1279,6 +1279,9 @@ TC_INIT(const char *tablename)
 	return h;
 error:
 	TC_FREE(&h);
+	/* A different process changed the ruleset size, retry */
+	if (errno == EAGAIN)
+		goto retry;
 	return NULL;
 }
 
