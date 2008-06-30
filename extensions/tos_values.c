4ded0f11d382c37a57c04077f453b3978eeded17
Jan Engelhardt <jengelh@medozas.de>
2008-06-30 14:57:08 +0200

    libxt_TOS: make sure --set-tos value/mask is recognized
    Only when a 'stop' pointer is passed, the string may consist of more
    than just a number.
    
    Reported-by: Anonymous
    Signed-off-by: Jan Engelhardt <jengelh@medozas.de>
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- extensions/tos_values.c
+++ extensions/tos_values.c
@@ -56,8 +56,9 @@ static bool tos_parse_symbolic(const char *str, struct tos_value_mask *tvm,
 {
 	const unsigned int max = 255;
 	const struct tos_symbol_info *symbol;
+	char *tmp;
 
-	if (strtonum(str, NULL, NULL, 0, max))
+	if (strtonum(str, &tmp, NULL, 0, max))
 		return tos_parse_numeric(str, tvm, max);
 
 	/* Do not consider ECN bits */
