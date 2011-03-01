ee52e00adfb10250e1828b94e43d9482bb201827
Jan Engelhardt <jengelh@medozas.de>
2011-03-01 02:45:34 +0100

    iptables: fix an inversion
    Revisiting the original condition (viewable in git log -1 -p
    v1.4.10-57-gacef604), one can notice an unforuntate inversion. This
    commit corrects this.
    
    Testcase: -A INPUT -p tcp --dport 1
    
    Reported-by: Florian Westphal
    Signed-off-by: Jan Engelhardt <jengelh@medozas.de>

--- xshared.c
+++ xshared.c
@@ -87,7 +87,7 @@ static bool should_load_proto(struct iptables_command_state *cs)
 	if (cs->protocol == NULL)
 		return false;
 	if (find_proto(cs->protocol, XTF_DONT_LOAD,
-	    cs->options & OPT_NUMERIC, NULL) != NULL)
+	    cs->options & OPT_NUMERIC, NULL) == NULL)
 		return true;
 	return cs->proto_used;
 }
